c5d85fdf9afa0704acc78471a6f88783a30dd530
Sourav Batabyal <meetmesouravbatabyal@gmail.com>
2021-06-02 14:33:53 +0530

    revert app.js before assignment

--- src/App.js
+++ src/App.js
@@ -1,44 +1,88 @@
 import React, { Component } from "react";
 import "./App.css";
-import Char from "./Char/Char";
-import Validation from "./Validation/Validation";
+import Person from "./Person/Person";
 
 class App extends Component {
   state = {
-    inputText: "",
+    persons: [
+      { id: "adad", name: "Max", age: 28 },
+      { id: "edqewd", name: "Manu", age: 29 },
+      { id: "tyfatda", name: "Stephanie", age: 26 },
+    ],
+    otherState: "some other value",
+    showPersons: false,
   };
 
-  inputChangedHandler = (event) => {
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => p.id === id);
+    
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      inputText: event.target.value,
+      persons: persons
     });
   };
 
-  deleteCharHandler = (index) => {
-    const text = this.state.inputText.split('');
-    text.splice(index, 1);
-    const updatedText = text.join('');
-    this.setState({inputText: updatedText});
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   };
 
+  togglePersonsHandler = () => {
+    const show = this.state.showPersons;
+    this.setState({ showPersons: !show });
+  };
+
+  /*
+  () => this.switchNameHandler('Maximilian!!') can be more inefficient than bind. 
+  Try to use bind
+  */
   render() {
-    const chars = this.state.inputText.split("").map((t, index) => {
-      return <Char click={() => this.deleteCharHandler(index)} character={t} key={index} />;
-    });
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person 
+            click={() => this.deletePersonHandler(index)}
+            name={person.name} 
+            age={person.age}
+            key={person.id} 
+            changed={(event) => this.nameChangeHandler(event, person.id)}
+            />;
+          })}
+        </div>
+      );
+    }
 
     return (
       <div className="App">
-        <input
-          type="text"
-          onChange={this.inputChangedHandler}
-          value={this.state.inputText}
-        />
-        <p>{this.state.inputText}</p>
-        <p>{this.state.inputText.length}</p>
-        <Validation inputLength={this.state.inputText.length} />
-        {chars}
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button style={style} onClick={this.togglePersonsHandler}>
+          {this.state.showPersons ? "Hide Persons" : "Show Persons"}
+        </button>
+        {persons}
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
